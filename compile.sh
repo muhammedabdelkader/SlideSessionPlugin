e468f472faf182e44f392791561185bf5dc9f32b
Muhammed M.Bassem AbdElKader <56849576+muhammedabdelkader@users.noreply.github.com>
2022-06-16 02:27:27 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@ rm -rf burp.jar
 cd out
 cd production 
 cd slideSessionPlugin
-jar cf  ../../../burp.jar ./burp/mainfest.txt ./com/slideSessionPlugin/data/*.class ./burp/*.class ./com/slideSessionPlugin/gui/*.class ./com/slideSessionPlugin/json/*.class ./com/slideSessionPlugin/logic/*.class
+jar cf  ../../../release/SSP.jar ./burp/mainfest.txt ./com/slideSessionPlugin/data/*.class ./burp/*.class ./com/slideSessionPlugin/gui/*.class ./com/slideSessionPlugin/json/*.class ./com/slideSessionPlugin/logic/*.class
