699a84576ed373c876314488953872fcb1a78a23
LE570 <muhammed.m.abdelkader@gmail.com>
2022-06-16 05:15:57 -0700

    Feat(SSP): release 1.0

--- cleanPush.sh
+++
@@ -1,7 +0,0 @@
-echo -ne "Prepare Push"
-rm -rf out/
-rm -rf *.jar 
-git add . 
-git commit -m "commit date: "+date
-git push 
-echo -ne "Finished " 
--- compile.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-
-rem del burp.jar
-cd out
-cd production 
-cd slideSessionPlugin
-jar cf  ../../../burp.jar ./burp/mainfest.txt ./com/slideSessionPlugin/data/*.class ./burp/*.class ./com/slideSessionPlugin/gui/*.class ./com/slideSessionPlugin/json/*.class ./com/slideSessionPlugin/logic/*.class
-
